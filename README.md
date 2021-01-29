a27426406f498b10a9a44bd3f186c4fe8bb7d675
eForce-Tsai <ef618885@gmail.com>
2021-01-29 13:29:50 +0800

    eForce-IR

---
+++ README.md
@@ -0,0 +1,14 @@
+ # eForce_IR for micro-bit
+
+Extension for eForce_IR
+
+Note: IR receiver for eForce receiver
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ enums.d.ts
@@ -0,0 +1,53 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum Pins {
+    P0 = 3,
+    P1 = 2,
+    P2 = 1,
+    P3 = 4,
+    P4 = 5,
+    P5 = 17,
+    P6 = 12,
+    P7 = 11,
+    P8 = 18,
+    P9 = 10,
+    P10 = 6,
+    P11 = 26,
+    P12 = 20,
+    P13 = 23,
+    P14 = 22,
+    P15 = 21,
+    P16 = 16,
+    P19 = 0,
+    P20 = 30,
+    }
+
+
+    declare enum RemoteButton {   
+      NUM1 = 0x45,
+	  NUM2 = 0x46,
+	  Num3 = 0x47,
+	  NUM4 = 0x44,
+	  NUM5 = 0x40,
+	  NUM6 = 0x43,
+	  NUM7 = 0x07,
+	  NUM8 = 0x15,
+	  NUM9 = 0x09,
+	  STAR = 0x16,
+	  NUM0 = 0x19,
+	  HASHTAG = 0x0d,
+	  NULL1 = 0x3,
+	  UP = 0x18,
+	  NULL2 = 0x5e,
+	  LEFT = 0x8,
+	  OK = 0x1c,
+	  RIGHT = 0x5a,
+	  NULL3 = 0x42,
+	  DOWN = 0x52,
+	  NULL4 = 0x4A,
+    }
+declare namespace eForce_IR {
+}
+
+// Auto-generated. Do not edit. Really.

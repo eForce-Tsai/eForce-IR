db54ffd6660bc6825de1cd1b5fff1a98f1d8c2d1
eForce-Tsai <ef618885@gmail.com>
2021-01-29 15:33:36 +0800

    eForce-IR 中文版

--- shims.d.ts
+++ shims.d.ts
@@ -10,14 +10,14 @@ declare namespace eForce_IR {
      * button pushed.
      */
     //% blockId=ir_received_left_event
-    //% block="on |%btn| button pressed" shim=Mbit_IR::onPressEvent
+    //% block="按下 |%btn| 按鈕" shim=Mbit_IR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
     /**
      * initialises local variablesssss
      */
     //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=Mbit_IR::init
+    //% block="將紅外接收器連接到接腳 %pin" shim=Mbit_IR::init
     function init(pin: Pins): void;
 }
 
